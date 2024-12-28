8404acf89f160281a43c958317ea0b54f6f4f8cc
john.baldonado <baldonadojm0216@gmail.com>
2024-12-28 22:53:34 +0800

    feature: add navbar component

---
+++ client/src/app/(components)/DashboardWrapper.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/app/(components)/Navbar";
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function DashboardWrapper({ children }: Readonly<Props>) {
+  return (
+    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      Sidebar
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+}
